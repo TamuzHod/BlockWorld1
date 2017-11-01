743286b984e9bfb4152316d9fd3fedc61c44842c
TamuzHod <tamuzhod@gmail.com>
2017-11-01 13:41:24 -0700

    This adds all the game files

---
+++ Project SpaceCube - Ep 3/Project SpaceCube - Ep 3/Assets/KeybinableComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeybinableComponent : MonoBehaviour {
+
+    public KeyCode keyCode { get; set;  }
+
+    public void SetKeybind( KeyCode kc )
+    {
+
+    } 
+}
---
+++ Project SpaceCube - Ep 3/Project SpaceCube - Ep 3/Assets/ToolbarBuildButtons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolbarBuildButtons : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+        MouseManager mouseManger = GameObject.FindObjectOfType<MouseManager>();
+
+        // populate our button list
+
+        foreach (GameObject shipPart in ShipPartPrefabs)
+        {
+            GameObject buttonGameObject = Instantiate(BuildButtonPrefab, this.transform);
+            Text buttonLabel = buttonGameObject.GetComponentInChildren<Text>();
+            buttonLabel.text = shipPart.name;
+
+            Button theButton = buttonGameObject.GetComponent<Button>();
+
+            theButton.onClick.AddListener( () => { mouseManger.ThePrefabToSpawn = shipPart; }  );
+
+        }
+    }
+
+
+    public GameObject BuildButtonPrefab;
+    public GameObject[] ShipPartPrefabs;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
